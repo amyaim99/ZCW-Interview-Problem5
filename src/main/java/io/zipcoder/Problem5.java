d5f9b6648793abf00751f0c5f8b0806f5d551cfb
amyaim <amyaim99@gmail.com>
2017-11-28 10:39:45 -0500

    commitingh

--- src/main/java/io/zipcoder/Problem5.java
+++ src/main/java/io/zipcoder/Problem5.java
@@ -1,4 +1,33 @@
 package io.zipcoder;
 
+import java.util.stream.Stream;
+
 public class Problem5 {
+
+// I dont know how to make this work without using built in methods
+
+    public Character returnCharInAString(String string){
+
+
+        for(int i=0; i<string.length()-1; i++){
+
+        }
+
+
+
+    }
+
+
+
+
+
+
+    public static String reverseCapitalizeString(String string){
+
+
+        Stream.Builder
+
+
+        return string;
+    }
 }
--- src/test/java/io/zipcoder/Problem5Test.java
+++ src/test/java/io/zipcoder/Problem5Test.java
@@ -1,4 +1,17 @@
 package io.zipcoder;
 
 public class Problem5Test {
+    Problem5 problem5 = new Problem5();
+
+    //output = Gnitest TINU Eovl I
+
+
+    public static void reverseStringTest(){
+
+        String input = "I love UNIT Testing";
+
+        String expected ="Gnitest TINU Eovl I";
+
+        String actual =  Problem5.reverseCapitalizeString(input);
+    }
 }
